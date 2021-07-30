407defb9b606d8e67d0d161fd6c3b05f7e1175d0
joaomarcuscardoso <rfcjoujou@gmail.com>
2021-07-30 16:53:20 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -0,0 +1,82 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.educandoweb.course.entities.pk.OrderItemPK;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        super();
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setOrder(Product product) {
+        id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof OrderItem)) {
+            return false;
+        }
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
